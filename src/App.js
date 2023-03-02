75ae6730875a6a42735b72129da02ebf8c4dcf30
rubi <rubi0624@nate.com>
2023-03-02 15:52:14 +0900

    feat: web3 modal 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import React from "react";
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal, Web3Button } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+
+const chains = [arbitrum, mainnet, polygon];
+
+// Wagmi client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: "04f0e423a8a4d3c890262be83c0e98b5" }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: "web3Moda", chains }),
+  provider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <Web3Button />
+      </WagmiConfig>
+
+      <Web3Modal
+        projectId="04f0e423a8a4d3c890262be83c0e98b5"
+        theme="dark"
+        accentColor="default"
+        ethereumClient={ethereumClient}
+      />
+    </>
+  );
 };
 
 export default App;
